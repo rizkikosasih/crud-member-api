0d3c24ac814d46b45a484d3aa67be863987c5807
Rizki Kosasih <rizkikosasih1997@gmail.com>
2025-10-27 17:52:27 +0700

    add routes api for register, login, logout, and users

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+
+    Route::apiResource('users', UserController::class);
+
+    Route::post('logout', [AuthController::class, 'logout']);
 });
+
