e462c18acf506e8b306009eb15570aa8059eeea0
Rizki Kosasih <rizkikosasih1997@gmail.com>
2025-10-27 17:18:51 +0700

    make migration and model hobby

---
+++ app/Models/Hobby.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hobby extends Model
+{
+    protected $fillable = ['user_id', 'name'];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
