3b8e825253e218ecb2e30ea4883ebb954eff3419
Rizki Kosasih <rizkikosasih1997@gmail.com>
2025-10-28 13:55:51 +0700

    update routes web for authentication, users, and home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AuthController::class, 'login'])->name('index');
+Route::get('login', [AuthController::class, 'login'])->name('login');
+
+Route::get('register', [AuthController::class, 'register'])->name('register');
+
+Route::get('/home', fn() => view('home'))->name('home');
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/edit/{id}', [UserController::class, 'edit'])->name('users.edit')->where('id', '[0-9]+');
 });
