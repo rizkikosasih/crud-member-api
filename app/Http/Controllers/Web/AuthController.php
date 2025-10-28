fbe6f436d758b9539fa7653fdf04fb0bba7f92f8
Rizki Kosasih <rizkikosasih1997@gmail.com>
2025-10-28 13:57:42 +0700

    remove unused service

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -3,17 +3,10 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\Services\AuthApiService;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    protected AuthApiService $authApi;
-
-    public function __construct(AuthApiService $authApi)
-    {
-        $this->authApi = $authApi;
-    }
 
     public function login()
     {
