cf3ef6361172695e1417ca395d2a06ec1f58d4ba
Rizki Kosasih <rizkikosasih1997@gmail.com>
2025-10-28 13:48:56 +0700

    use traits HasFactory

--- app/Models/Hobby.php
+++ app/Models/Hobby.php
@@ -2,17 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Hobby extends Model
 {
+    use HasFactory;
     protected $fillable = ['user_id', 'name'];
 
     protected $casts = [
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s',
     ];
-
 
     public function user()
     {
