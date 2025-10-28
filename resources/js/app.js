a8e76a7d86f47d07095a95b3d2abf0f5fc574412
Rizki Kosasih <rizkikosasih1997@gmail.com>
2025-10-28 13:52:37 +0700

    setup ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-import './bootstrap';
+import axios from 'axios';
+
+window.axios = axios;
+
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '/api';
+axios.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => Promise.reject(error));
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.status === 401) {
+            // token invalid / expired
+            localStorage.removeItem('token');
+
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
