784a99c0276cdf07b63ff9ed10729f758d325b08
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-04 00:48:43 +0700

    feat(account): add ChangePasswordRequest for password change validation

--- app/Http/Requests/Account/ChangePasswordRequest.php
+++ app/Http/Requests/Account/ChangePasswordRequest.php
@@ -1,16 +1,11 @@
 <?php
 
-namespace App\Http\Requests\Auth;
+namespace App\Http\Requests\Account;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class ChangePasswordRequest extends FormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     public function rules(): array
     {
         return [
